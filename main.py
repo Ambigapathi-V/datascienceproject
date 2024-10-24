71c4cb510724def77597f83a0ceea7c28436f263
Ambigapath <168761904+Ambigapath@users.noreply.github.com>
2024-10-24 16:35:47 +0530

    added model training

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DatavalidationTrainingPipeline
 from src.datascience.pipeline.data_transfromation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
+
 
 
 try:
@@ -32,6 +35,18 @@ try:
         logger.info(f">>>>>>{STAGE_NAME} started<<<<<<<")
         data_transformation = DataTransformationTrainingPipeline()
         data_transformation.initiate_data_transformation()
+        logger.info(f">>>>>>{STAGE_NAME} completed<<<<<<")
+except Exception as e:
+        logger.error(f">>>>>>Error in {STAGE_NAME}: {str(e)}>>>>>>")
+        raise e
+
+STAGE_NAME = "Model  Trainer stage"
+
+
+try:
+        logger.info(f">>>>>>{STAGE_NAME} started<<<<<<<")
+        modeltrainer = ModelTrainerTrainingPipeline()
+        modeltrainer.initiate_model_training()
         logger.info(f">>>>>>{STAGE_NAME} completed<<<<<<")
 except Exception as e:
         logger.error(f">>>>>>Error in {STAGE_NAME}: {str(e)}>>>>>>")
